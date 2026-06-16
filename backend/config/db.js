9e0c8458bed588a3ba680ef0674718ad6ce56841
Prathish <prathishs286@gmail.com>
2026-06-16 20:56:48 +0530

    backend modification

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+const mongoose=require("mongoose");
+
+const connectDB=async()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+
+        console.log("MongoDB Connected"+process.env.PORT);
+    } catch (error) {
+        console.log("MongoDB Connection Failed");
+    }
+};
+module.exports=connectDB;
---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const connectDB = require("./config/db");
+
+connectDB();
+
+const app = express();
+
+app.use(cors(
+    {origin:["http://localhost:5173","https://atm-mern-project.vercel.app/"]}
+));
+app.use(express.json());
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+
+  res.status(500).json({
+    message: err.message,
+  });
+});
+app.use("/api/atm", require("./routes/atmRoutes"));
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server Running");
+});
