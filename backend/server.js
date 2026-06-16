ba651a5cc63cd61a9cb2d41752ea09d854437abd
Prathish <prathishs286@gmail.com>
2026-06-16 21:12:22 +0530

    new modification

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ connectDB();
 const app = express();
 
 app.use(cors(
-    {origin:["http://localhost:5173","https://atm-mern-project.vercel.app/"]}
+    {origin:["http://localhost:5173","https://atm-mern-project.vercel.app"]}
 ));
 app.use(express.json());
 
